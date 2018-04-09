7d1b8305c21be59e431fb96862b0af423906e826
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-04-08 21:55:08 -0700

    Set up views. Configuring server file to work with handlebars.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+var exphbs = require('express-handlebars');
 
 // Require all models
 var db = require("./models");
@@ -30,20 +31,25 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+  // useMongoClient: true *no longer necessary*
 });
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main.handlebars' }));
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 
 // Routes
 
 // A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.cnet.com/news/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h3 ").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
